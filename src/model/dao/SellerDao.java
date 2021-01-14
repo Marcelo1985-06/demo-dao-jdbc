d1a37800f9708911d3dcbd638b47828b17f157b2
Marcelo1985-06 <marceloliveira_lima@outlook.com>
2021-01-14 16:59:01 -0300

    Insert implementation

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -1,5 +1,6 @@
 package model.dao;
 
+import db.DbException;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -7,7 +8,7 @@ import java.util.List;
 
 public interface SellerDao {
 
-    void insert(Seller obj); //Operacão responsável por inserir no banco de dados o objeto Seller.
+    void insert(Seller obj) throws DbException; //Operacão responsável por inserir no banco de dados o objeto Seller.
     void update(Seller obj);
     void deleteById(Integer id);
     Seller findById(Integer id);
